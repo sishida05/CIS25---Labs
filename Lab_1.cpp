e2978113689517a040f918aac8e466c51eabc33a
sishida05 <30080170@cc.peralta.edu>
2026-01-26 19:47:06 +0000

    Add Lab_1

---
+++ Lab_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int points;
+string name;
+
+points = 100;
+name = "Sean";
+
+cout << "Hello, " << name << "! The computer has seen fit to assign you " << points << " points." << endl;
+
+return 0;
+}
